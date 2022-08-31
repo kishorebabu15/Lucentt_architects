7a025d51701deb1da4a4f2c625e27499f7b6a75d
Kishore Babu <polurukishorebabu@gmail.com>
2022-08-31 21:39:08 +0530

    Database Links updated

--- Backend/index.js
+++ Backend/index.js
@@ -3,8 +3,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const ClientModel = require("./models/client")
+const cors = require("cors");
 
 const PORT = process.env.PORT || 8000;
+
+
+// Json -> object
+app.use(express.json())
+// allows to connect API to react
+app.use(cors());
 
 // DATABASE CONNECTION
 
@@ -17,5 +25,24 @@ mongoose.connect(process.env.DATABASE_LINK, {
     })
     .catch((error) => { console.log(error) });
 
-    
+app.get('/getClients', (req,res) => {
+    ClientModel.find({}, (error,result) => {
+        if(error) {
+            res.json(error);
+        } else {
+            res.json(result);
+        }
+    })
+})
+
+app.post("/createClient", async (req,res) => {
+    const newClient = new ClientModel(req.body);
+
+    await newClient.save();
+
+    res.json(req.body)
+})
+
+
+
 app.listen(PORT, console.log(`Server is Up and running at ${PORT}`));
---
+++ Backend/models/client.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+
+var clientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 32,
+        required: true,
+        trim: true
+    },
+    phone: {
+        type: Number,
+        maxlength: 10,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        maxlength: 32,
+        trim: true,
+    },
+    client_address: {
+        type: String,
+        maxlength: 255,
+        required: true,
+        trim: true
+    },
+    site_information: {
+        type: String,
+        maxlength: 255,
+        required: true,
+        trim: true,
+    }
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Client", clientSchema);
+
